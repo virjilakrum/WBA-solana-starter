6822da0fa96eb0909a831af1207f1d589430774e
Virjilakrum <asesenep15@gmail.com>
2024-08-03 10:40:53 +0300

    Update nft_image.ts

--- ts/cluster1/nft_image.ts
+++ ts/cluster1/nft_image.ts
@@ -19,7 +19,7 @@ umi.use(signerIdentity(signer));
 
 (async () => {
   try {
-    const image = await readFile("./cluster1/assets/image.jpeg");
+    const image = await readFile("./cluster1/assets/image.jpeg"); // change with nonlocal dir.
     const generic = await createGenericFile(image, "rug", {
       contentType: "image/png",
     });
